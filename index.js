9750621f02c981a1009e88004b44747434fe2ee8
Benjamin Heinle <bheinle7@gmail.com>
2017-06-06 22:27:49 -0500

    Remove form, Add image to auto-completed queris and snippet

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-m$(document).ready(function() {
-  $("#city").keyup(function() {
-    var search = $("#city").val();
-    //alert(search);
+$(document).ready(function() {
+  $("#input").keyup(function() {
+    var search = $("#input").val();
     var wikiSearchUrl =
       "https://en.wikipedia.org/w/api.php?action=opensearch&search=" +
       search +
-      "&format=json&callback=wikiCallback";
+      "&prop=pageimages&format=json&callback=wikiCallback";
+    var wikiSearchUrl1 = "https://en.wikipedia.org/w/api.php?action=query&formatversion=2&list=search&utf8=1&srsearch=" + search;
     $.ajax({
       url: wikiSearchUrl,
       dataType: "jsonp",
       success: function(searchResults) {
-        $("#wiki0").html(searchResults[1][1]);
-        $("#wiki1").html(searchResults[1][2]);
-        $("#wiki2").html(searchResults[1][3]);
+        $("#img1").append('<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/' + search + '.jpg/80px-' + search + '.jpg" alt="poo"/>'); //picture
+        $("p:first").text(searchResults[1][0]); //title
+        $("p:last").text(searchResults[2][0]); //snippet
       }
     });
   });
